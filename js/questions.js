3a88b40568232158df0d2e8a638a827d60111a29
Prashant K Bhardawaj <sempaiprashant@gmail.com>
2021-12-26 22:29:06 +0530

    quiz app
    This quiz app developed in javascript.

---
+++ js/questions.js
@@ -0,0 +1,32 @@
+let questions = [
+  {
+    numb: 1,
+    question: "Which animal is the national animal of India??",
+    answer: "Tiger",
+    options: ["Elephant", "Goat", "Zebra", "Tiger"],
+  },
+  {
+    numb: 2,
+    question: "What is the National Currency of India?",
+    answer: "Rupee",
+    options: ["Dollor", "Rupee", "Euro", "Yen"],
+  },
+  {
+    numb: 3,
+    question: "Which is the national sport of India?",
+    answer: "Hockey",
+    options: ["Hockey", "Cricket", "Boxing", "Tennis"],
+  },
+  {
+    numb: 4,
+    question: "Which state in India has the highest population?",
+    answer: "Uttar Pradesh",
+    options: ["Gujrat", "Bihar", "Uttar Pradesh", "Karnatak"],
+  },
+  {
+    numb: 5,
+    question: "Which city is known as the Pink City of India?",
+    answer: "Jaipur",
+    options: ["Pune", "Kolkata", "Patna", "Jaipur"],
+  },
+];
